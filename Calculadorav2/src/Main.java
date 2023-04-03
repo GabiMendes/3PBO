7611495833c25b3806891ff0144fe84b167773a3
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-04-03 00:44:27 -0300

    Segunda versão de "Calculadora Java".
    Nessa versão, você pode:
    
    Calcular raízes diversas;
    Realizar cálculos trigonométricos.
    
    Além das funções anteriores como Soma, Subtração, Multiplicação, Divisão, Cálculo de potência e Concatenação.

---
+++ Calculadorav2/src/Main.java
@@ -0,0 +1,113 @@
+import br.edu.faeterj.Calculadora;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)    {
+
+        Calculadora calc = new Calculadora();
+        Scanner sc = new Scanner(System.in);
+
+        long raizArredondada = 0;
+
+        System.out.println("Calculadora Jarvis, a calculadora feita em Java :) \n Regras de Uso: \n Insira primeiro valor, aperte Enter. \n Insira o operador, aperte Enter. \n Finalmente, insira o segundo valor e aperte Enter para ver o resultado. \n Informações Úteis: c - concatenação, p - potência, r - raiz, t - cálculos trigonométricos. ");
+
+        int valor1 = 0;
+        int valor2 = 0;
+        String svalor2 = "";
+
+        valor1 = sc.nextInt();
+
+        sc.nextLine();
+
+        char operador = sc.next().charAt(0);
+
+        sc.nextLine();
+
+        switch(operador)
+        {
+            case '+':
+
+                valor2 = sc.nextInt();
+
+                int resultadoSoma = calc.Soma(valor1, valor2);
+
+                System.out.println("Resultado:  " + resultadoSoma);
+
+                break;
+
+            case '-':
+
+                valor2 = sc.nextInt();
+
+                int resultadoSub = calc.Sub(valor1, valor2);
+
+                System.out.println("Resultado:  " + resultadoSub);
+
+                break;
+
+            case '/':
+
+                valor2 = sc.nextInt();
+
+                int resultadoDivi = calc.Divi(valor1, valor2);
+
+                System.out.println("Resultado:  " + resultadoDivi);
+
+                break;
+
+            case '*':
+
+                valor2 = sc.nextInt();
+
+                int resultadoMult = calc.Mult(valor1, valor2);
+
+                System.out.println("Resultado:  " + resultadoMult);
+
+                break;
+
+            case 'p':
+
+                valor2 = sc.nextInt();
+
+                int resultadoPot = calc.Pot(valor1, valor2);
+
+                System.out.println("Resultado:  " + resultadoPot);
+
+                break;
+
+            case 'r':
+
+                valor2 = sc.nextInt();
+
+                double resultadoRaiz = calc.Raiz(valor1, valor2);
+                raizArredondada = Math.round(resultadoRaiz);
+
+                System.out.println("Resultado:  " + raizArredondada);
+
+                break;
+
+            case 'c':
+
+                valor2 = sc.nextInt();
+
+                System.out.println("" + valor1 + "" + valor2);
+
+                break;
+
+            case 't':
+
+                System.out.println("Digite S para Seno, C para Cosseno e T para Tangente:  ");
+
+                svalor2 = sc.next();
+
+                double resultadoTrig = calc.Trig(valor1, svalor2);
+
+                System.out.println("Resultado:  " + resultadoTrig);
+
+                break;
+
+        }
+
+    sc.close();
+
+    }
+}

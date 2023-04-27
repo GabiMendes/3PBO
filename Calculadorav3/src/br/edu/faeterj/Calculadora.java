db578ceb13e2c28f6918b641babcce8b6d7bd458
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-04-27 11:11:08 -0300

    Quarta versão da Calculadora Java
    Métodos de operação foram passados de PÚBLICOS para PRIVADOS.
    
    Classe estática "DecideOperacao" foi CRIADA.
    
    Bugs da versão:
    
    A versão apresenta BUG no que compete o cálculo trigonométrico, a ser revisado.

--- Calculadorav3/src/br/edu/faeterj/Calculadora.java
+++ Calculadorav3/src/br/edu/faeterj/Calculadora.java
@@ -1,45 +1,119 @@
 package br.edu.faeterj;
-
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Calculadora
 {
-    public static int Soma(int a, int b)
+	public static double DecideOperacao(double valor1, double valor2, String op)
+	{
+	
+	double resultado = 0;
+	 double resultadoRaiz = 0;
+     String svalor2 = "";
+    long raizArredondada = 0;
+    
+	
+	switch(op)
     {
-        return a+b;
+        case "+":
+
+            resultado = Calculadora.Soma(valor1, valor2);
+
+            break;
+
+        case "-":
+
+            resultado = Calculadora.Sub(valor1, valor2);
+
+            break;
+
+        case "/":
+
+            resultado = (float)Calculadora.Divi(valor1, valor2);
+
+            break;
+
+        case "*":
+
+            resultado = Calculadora.Mult(valor1, valor2);
+
+            break;
+
+        case "p":
+
+            resultado = Calculadora.Pot(valor1, valor2);
+
+            break;
+
+        case "r":
+
+            Calculadora.Raiz(valor1, valor2);
+
+            resultadoRaiz = Calculadora.Raiz(valor1, valor2);
+
+            if(Math.round(resultadoRaiz)==resultadoRaiz)
+            {
+            	resultado=(int)resultadoRaiz;
+            }
+            else
+            {
+                resultado=-1;
+            }
+
+            break;
+
+        case "t":
+
+            System.out.println("Digite S para Seno, C para Cosseno e T para Tangente:  ");
+            Scanner sc = new Scanner(System.in);
+            svalor2 = sc.next();
+
+            resultado = (int)Calculadora.Trig(valor1, svalor2);
+
+            break;
+
+    }
+	
+	return resultado;
+	
+	}
+	
+    private static double Soma(double valor1, double valor2)
+    {
+        return valor1+valor2;
     }
 
-    public static int Mult(int a, int b)
+    private static double Mult(double valor1, double valor2)
     {
-        return a*b;
+        return valor1*valor2;
     }
 
-    public static int Divi(int a, int b)
+    private static double Divi(double valor1, double valor2)
     {
-        return a/b;
+        return valor1/valor2;
     }
 
-    public static int Sub(int a, int b)
+    private static double Sub(double valor1, double valor2)
     {
-        return a-b;
+        return valor1-valor2;
     }
 
-    public static int Pot(int a, int b)
+    private static int Pot(double valor1, double valor2)
     {
-        return (int) Math.pow(a, b);
+        return (int) Math.pow(valor1, valor2);
     }
 
-    public static double Raiz(float a, float b)
+    private static double Raiz(double valor1, double valor2)
     {
-        double ResultadoRaiz = Math.pow(a,(1/b));
+        double ResultadoRaiz = Math.pow(valor1,(1/valor2));
 
-        return (double) Math.pow(a,(1/b));
+        return (double) Math.pow(valor1,(1/valor2));
 
     }
 
-    public static double Trig(int a, String b )
+    private static double Trig(double valor1, String b )
     {
-        double angRad = a * Math.PI / 180;
+        double angRad = valor1 * Math.PI / 180;
         double resultado = 0;
 
         if((Objects.equals(b, "S"))||(Objects.equals(b, "s")))

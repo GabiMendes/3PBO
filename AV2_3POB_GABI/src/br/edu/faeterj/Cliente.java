20f43370696e3ed22c6213c44bf8437c2112ff37
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-07-06 11:37:34 -0300

    Update Cliente.java
    A SER CONSERTADO:
    
    ID DE CLIENTE.

--- AV2_3POB_GABI/src/br/edu/faeterj/Cliente.java
+++ AV2_3POB_GABI/src/br/edu/faeterj/Cliente.java
@@ -1,82 +1,81 @@
 package br.edu.faeterj;
 
 public class Cliente {
-	
-	private int id;
-	private String nome;
-	private String endereco;
-	private String postalCode;
-	public String pais;
-	private int cpf;
-	private int passaporte;
-	public String email;
-	public String dataNascimento;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	public String getPostalCode() {
-		return postalCode;
-	}
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-	public String getPais() {
-		return pais;
-	}
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
-	public int getCpf() {
-		return cpf;
-	}
-	public void setCpf(int cpf) {
-		this.cpf = cpf;
-	}
-	public int getPassaporte() {
-		return passaporte;
-	}
-	public void setPassaporte(int passaporte) {
-		this.passaporte = passaporte;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	
-	public Cliente(int id, String nome, String endereco, String postalCode, String pais, int cpf, int passaporte, String email, String dataNascimento) {
-		this.id = id;
-		this.nome = nome;
-		this.endereco = endereco;
-		this.postalCode = postalCode;
-		this.pais = pais;
-		this.cpf = cpf;
-		this.passaporte = passaporte;
-		this.email = email;
-		this.dataNascimento = dataNascimento;
-	}
-	
+
+    private static int id;
+    private String nome;
+    private String endereco;
+    private String postalCode;
+    public String pais;
+    private int cpf;
+    private int passaporte;
+    public String email;
+    public String dataNascimento;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public int getPassaporte() {
+        return passaporte;
+    }
+    public void setPassaporte(int passaporte) {
+        this.passaporte = passaporte;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Cliente(int id, String nome, String endereco, String postalCode, String pais, int cpf, int passaporte, String email, String dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.postalCode = postalCode;
+        this.pais = pais;
+        this.cpf = cpf;
+        this.passaporte = passaporte;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
 }

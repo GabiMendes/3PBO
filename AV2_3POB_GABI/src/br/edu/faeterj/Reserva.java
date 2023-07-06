b557694aef6841ce4da0ba08d9ba8be9446649dd
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-07-06 11:39:11 -0300

    Update Reserva.java
    FUNÇÃO PARA EXIBIR DETALHES DE RESERVA EM CONSTRUÇÃO.

--- AV2_3POB_GABI/src/br/edu/faeterj/Reserva.java
+++ AV2_3POB_GABI/src/br/edu/faeterj/Reserva.java
@@ -2,58 +2,71 @@ package br.edu.faeterj;
 
 public class Reserva {
 
-	private int id;
-	public int idQuarto;
-	public int idCama;
-	public int idCliente;
-	public String dataEntrada;
-	public String dataSaida;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getIdQuarto() {
-		return idQuarto;
-	}
-	public void setIdQuarto(int idQuarto) {
-		this.idQuarto = idQuarto;
-	}
-	public int getIdCama() {
-		return idCama;
-	}
-	public void setIdCama(int idCama) {
-		this.idCama = idCama;
-	}
-	public int getIdCliente() {
-		return idCliente;
-	}
-	public void setIdCliente(int idCliente) {
-		this.idCliente = idCliente;
-	}
-	public String getDataEntrada() {
-		return dataEntrada;
-	}
-	public void setDataEntrada(String dataEntrada) {
-		this.dataEntrada = dataEntrada;
-	}
-	public String getDataSaida() {
-		return dataSaida;
-	}
-	public void setDataSaida(String dataSaida) {
-		this.dataSaida = dataSaida;
-	}
-	
-	public Reserva(int id, int idQua) {
-		
-	}
-	
-	private int id;
-	public int idQuarto;
-	public int idCama;
-	public int idCliente;
-	public String dataEntrada;
-	public String dataSaida;
+    private int id;
+    public int idQuarto;
+    public int idCama;
+    public int idCliente;
+    public String dataEntrada;
+    public String dataSaida;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getIdQuarto() {
+        return idQuarto;
+    }
+    public void setIdQuarto(int idQuarto) {
+        this.idQuarto = idQuarto;
+    }
+    public int getIdCama() {
+        return idCama;
+    }
+    public void setIdCama(int idCama) {
+        this.idCama = idCama;
+    }
+    public int getIdCliente() {
+        return idCliente;
+    }
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+    public String getDataEntrada() {
+        return dataEntrada;
+    }
+    public void setDataEntrada(String dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+    public String getDataSaida() {
+        return dataSaida;
+    }
+    public void setDataSaida(String dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Reserva(int id, int idQua) {
+
+    }
+
+    public Reserva(int id, int idQuarto, int idCama, int idCliente, String dataEntrada, String dataSaida)
+    {
+        this.id = id;
+        this.idQuarto = idQuarto;
+        this.idCama = idCama;
+        this.idCliente = idCliente;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    //Trabalhar nos detalhes abaixo
+    public String detReserva() {
+        String result = "ID: " + id +
+                ", ID do Quarto: " + Quarto.getId() +
+                ", ID da Cama: " + Cama.getId() +
+                ", ID do Cliente: " + Cliente.getId() +
+                ", Data de Entrada: " + dataEntrada +
+                ", Data de Saída: " + dataSaida;
+    }
 }

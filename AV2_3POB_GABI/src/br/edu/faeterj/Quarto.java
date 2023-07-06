f676ceedbf261c14939602aca9e889e8e2f77d8c
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-07-06 11:38:36 -0300

    Update Quarto.java
    DESENVOLVIMENTO DE CLASSE "QUARTO".
    
    ATRIBUTOS ATRIBUÍDOS.
    
    GETTERS E SETTERS ATRIBUÍDOS.

--- AV2_3POB_GABI/src/br/edu/faeterj/Quarto.java
+++ AV2_3POB_GABI/src/br/edu/faeterj/Quarto.java
@@ -2,4 +2,57 @@ package br.edu.faeterj;
 
 public class Quarto {
 
+    private int id;
+    public String nomeQuarto;
+    public int qtdeCamas;
+    public boolean temBanheiro;
+    public String descricao;
+
+    public static int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeQuarto() {
+        return nomeQuarto;
+    }
+
+    public void setNomeQuarto(String nomeQuarto) {
+        this.nomeQuarto = nomeQuarto;
+    }
+
+    public int getQtdeCamas() {
+        return qtdeCamas;
+    }
+
+    public void setQtdeCamas(int qtdeCamas) {
+        this.qtdeCamas = qtdeCamas;
+    }
+
+    public boolean isTemBanheiro() {
+        return temBanheiro;
+    }
+
+    public void setTemBanheiro(boolean temBanheiro) {
+        this.temBanheiro = temBanheiro;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Quarto(int id, String nomeQuarto, int qtdeCamas, boolean temBanheiro, String descricao) {
+        this.id = id;
+        this.nomeQuarto = nomeQuarto;
+        this.qtdeCamas = qtdeCamas;
+        this.temBanheiro = temBanheiro;
+        this.descricao = descricao;
+    }
 }

ca43bb3580ff284160b0fe293b1032257f0d666d
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-07-06 11:38:06 -0300

    Update Cama.java
    DESENVOLVIMENTO DE CLASSE "CAMA".
    
    ATRIBUTOS ATRIBUÍDOS.
    
    GETTERS E SETTERS ATRIBUÍDOS.

--- AV2_3POB_GABI/src/br/edu/faeterj/Cama.java
+++ AV2_3POB_GABI/src/br/edu/faeterj/Cama.java
@@ -2,4 +2,57 @@ package br.edu.faeterj;
 
 public class Cama {
 
+    private int id;
+    private String codigoCama;
+    public boolean ehBeliche;
+    public String posicao;
+    public String descricao;
+
+    public static int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigoCama() {
+        return codigoCama;
+    }
+
+    public void setCodigoCama(String codigoCama) {
+        this.codigoCama = codigoCama;
+    }
+
+    public boolean isEhBeliche() {
+        return ehBeliche;
+    }
+
+    public void setEhBeliche(boolean ehBeliche) {
+        this.ehBeliche = ehBeliche;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Cama(int id, String codigoCama, boolean ehBeliche, String posicao, String descricao) {
+        this.id = id;
+        this.codigoCama = codigoCama;
+        this.ehBeliche = ehBeliche;
+        this.posicao = posicao;
+        this.descricao = descricao;
+    }
 }
